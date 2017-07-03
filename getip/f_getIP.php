463bc29055014c37f741e5955d9495389c7109f1
xbride <mail@yfhcw.net>
2017-07-04 00:31:20 +0800

    f_getip.php add

---
+++ getip/f_getIP.php
@@ -0,0 +1,34 @@
+<?php
+function getIP()
+{
+	echo "11"
+	return "1";
+}
+
+function getIP_()
+{
+    static $realip;
+    if (isset($_SERVER)){
+        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])){
+            $realip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        } else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+            $realip = $_SERVER["HTTP_CLIENT_IP"];
+        } else {
+            $realip = $_SERVER["REMOTE_ADDR"];
+        }
+    } else {
+        if (getenv("HTTP_X_FORWARDED_FOR")){
+            $realip = getenv("HTTP_X_FORWARDED_FOR");
+        } else if (getenv("HTTP_CLIENT_IP")) {
+            $realip = getenv("HTTP_CLIENT_IP");
+        } else {
+            $realip = getenv("REMOTE_ADDR");
+        }
+    }
+    return $realip;
+}
+
+//www.111cn.net
+	echo getIP_();
+?>
+
